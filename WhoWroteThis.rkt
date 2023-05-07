83b0a1f4a0013e7b9eb55f1b2bfb1e6adc25aa2e
Tony Russell <36004639+tony-06@users.noreply.github.com>
2023-05-07 00:02:17 -0500

    Update WhoWroteThis.rkt

--- WhoWroteThis.rkt
+++ WhoWroteThis.rkt
@@ -1,41 +1,27 @@
 #lang racket
 
-(define (read-file-as-string file) ;open file and port to string, port automatically closes
-  (clean-string (port->string (open-input-file file))))
+(define (make-string-hash string-lst) ;this makes my assignment 2 function look bloated
+  (define count-words (map (lambda (x) (cons x (count (curry equal? x) string-lst))) string-lst))
+  (define total-words (apply + (map cdr count-words)))
+  (define frequencies
+    (map (lambda (p) (cons (car p) (- 10(* -1 (log (/ (cdr p) total-words) 10))))) count-words))
+  (make-immutable-hash frequencies))
 
-(define (clean-string input-string) ;remove special chars and switch to lower case
-  (define (replace-chars string)
-    (regexp-replaces string '([#rx"[-‒—“!:;,.?\n\r]" " "]
-                              [#rx"\"" " "])))
-  (string-split (string-downcase(replace-chars input-string)))) ; returns list of clean substrings
-
-(define (make-string-hash string-lst hash-table)
-  (define (get-word-count lst) (count (curry equal? (first lst)) lst)) ;word count for each word
-  (hash-set! hash-table (first string-lst) (get-word-count string-lst))
-
-  (if (not (empty? (rest string-lst))) ;recursive call for the rest of the list
-      (make-string-hash (filter-not (curry equal? (first string-lst)) string-lst) hash-table)
-      (normalized-relative-frequency-hash hash-table)))
-
-(define (normalized-relative-frequency-hash hash-table) ;update hash values with frequencies
-  (define word-count (apply + (hash-values hash-table)))
-  (hash-map hash-table (lambda (k v) (hash-set! hash-table k (* -1 (log (/ v word-count) 10))))))
-
-(define (compare-hash-tables known-author mystery-author) ;compare 2 hash tables
-  (define frequency-differences
-    (filter number?(hash-map mystery-author
-                             (lambda (k v)(when (hash-has-key? known-author k)
-                                     (abs (- v (hash-ref known-author k))))))))
-  (/ (foldl + 0 frequency-differences) (length frequency-differences)))
-  
 (define (main filename) ;create three hash tables and compare them
-  (define doyle-hash (make-hash))
-  (define lovecraft-hash (make-hash))
-  (define mystery-hash (make-hash))
-  (make-string-hash (read-file-as-string "Doyle.txt") doyle-hash)
-  (make-string-hash (read-file-as-string "Lovecraft.txt") lovecraft-hash)
-  (make-string-hash (read-file-as-string filename) mystery-hash)
-  (if (> (compare-hash-tables doyle-hash mystery-hash) (compare-hash-tables lovecraft-hash mystery-hash))
+  (define (clean-string input-string) ;remove special chars and switch to lower case
+    (define (replace-chars string)(regexp-replace* #px"[^[:alpha:][:digit:][:space:]]" string ""))
+    (string-split (string-downcase(replace-chars input-string)))) ; returns list of clean substrings
+  (define (compare-hash-tables known-author mystery-author) ;compare 2 hash tables
+    (define frequency-differences
+      (filter number?(hash-map mystery-author
+                               (lambda (k v)(when (hash-has-key? known-author k)
+                                              (abs (- v (hash-ref known-author k))))))))
+    (/ (foldl + 0 frequency-differences) (length frequency-differences)))
+  (define (read-file-as-string file) ;open file and port to string, port automatically closes
+    (clean-string (port->string (open-input-file file))))
+  (define mystery-hash (make-string-hash (read-file-as-string filename))) 
+  (if (> (compare-hash-tables (make-string-hash (read-file-as-string "Doyle.txt")) mystery-hash)
+         (compare-hash-tables (make-string-hash (read-file-as-string "Lovecraft.txt")) mystery-hash))
       (println "Probably Lovecraft")
       (println "Probably Doyle")))
 
